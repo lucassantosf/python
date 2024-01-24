e706d98bb8f350f91091b3c62918a94a6be2e53c
lucas <lucas46em@gmail.com>
2024-01-24 17:13:37 -0300

    init templates

--- aula-1/recipes/views.py
+++ aula-1/recipes/views.py
@@ -2,7 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 
 def home(request): 
-    return render(request, 'recipes/home.html', context={'name':'Lucas'})
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
+    return render(request, 'recipes/pages/home.html', context={'name':'Lucas'})
