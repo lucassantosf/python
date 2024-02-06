eea064535165c410237ae0f0b394ba0b2c5a9cd6
lucas <lucas46em@gmail.com>
2024-02-06 18:09:03 -0300

    dinamic templates

--- aula-1/recipes/views.py
+++ aula-1/recipes/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse 
 from utils.recipes.factory import make_recipe
 
-def home(request): 
-    return render(request, 'recipes/pages/home.html', context={'recipes':[make_recipe() for _ in range(20)]})
+from .models import Recipe
 
-def recipe(request, id): 
-    return render(request, 'recipes/pages/recipe-view.html', context={'recipe':make_recipe(),'is_detail_page':True})
+def home(request):
+    recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
+    })
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        category__id=category_id,
+        is_published=True,
+    ).order_by('-id')
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+    })
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True,
+    })
