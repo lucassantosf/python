589a1383f0825fadd24681235ee2de180f54dbec
lucas <lucas46em@gmail.com>
2024-02-16 14:23:58 -0300

    more tests

--- aula-1/recipes/tests/test_recipe_views.py
+++ aula-1/recipes/tests/test_recipe_views.py
@@ -27,6 +27,14 @@ class RecipeViewsTest(RecipeTestBase):
         self.assertIn('Recipe title',content) 
         self.assertEqual(len(response_context_recipes),1) 
 
+    def test_recipe_home_template_does_not_load_recipes_not_published(self):
+        """Test recipe is_published False doesnt show"""
+        self.make_recipe(is_published=False)
+
+        response = self.client.get(reverse('recipes:home'))
+
+        self.assertIn('<h1>Sem receitas</h1>',response.content.decode('utf-8'))
+
     def test_recipe_category_views_function_is_correct(self):
         view = resolve(
             reverse('recipes:category', kwargs={'category_id':1})
@@ -39,6 +47,24 @@ class RecipeViewsTest(RecipeTestBase):
         )
         self.assertEqual(response.status_code, 404)
 
+    def test_recipe_category_template_loads_recipes(self):
+        needed_title = 'This is a category test'
+        self.make_recipe(title=needed_title)
+
+        response = self.client.get(reverse('recipes:category',args=(1,)))
+        content = response.content.decode('utf-8')
+        response_context_recipes = response.context['recipes']
+
+        self.assertIn(needed_title,content) 
+
+    def test_recipe_category_template_does_not_load_recipes_not_published(self):
+        """Test recipe is_published False doesnt show"""
+        recipe = self.make_recipe(is_published=False)
+
+        response = self.client.get(reverse('recipes:recipe',kwargs={'id':recipe.category.id}))
+
+        self.assertEqual(response.status_code,404)
+
     def test_recipe_detail_view_returns_404_if_no_recipes_found(self):
         response = self.client.get(
             reverse('recipes:recipe', kwargs={'id':1000})
@@ -49,3 +75,19 @@ class RecipeViewsTest(RecipeTestBase):
         view = resolve(reverse('recipes:recipe', kwargs={'id':1}))
         self.assertIs(view.func, views.recipe)
 
+    def test_recipe_detail_template_loads_the_corret_recipe(self):
+        needed_title = 'This is a detail page'
+        self.make_recipe(title=needed_title)
+
+        response = self.client.get(reverse('recipes:recipe',kwargs={'id':1}))
+        content = response.content.decode('utf-8')
+
+        self.assertIn(needed_title,content) 
+
+    def test_recipe_detail_template_does_not_load_recipe_not_published(self):
+        """Test recipe is_published False doesnt show"""
+        recipe = self.make_recipe(is_published=False)
+
+        response = self.client.get(reverse('recipes:recipe',kwargs={'id':recipe.id}))
+
+        self.assertEqual(response.status_code,404)
