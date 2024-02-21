673687ba37dae340249a720359aa9123fd7da30b
lucas <lucas46em@gmail.com>
2024-02-21 12:58:16 -0300

    refactor tests

--- aula-1/recipes/views.py
+++ aula-1/recipes/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from utils.recipes.factory import make_recipe
@@ -31,9 +32,21 @@ def recipe(request, id):
     })
 
 def search(request):
-    search_term = request.GET.get('q')
+    search_term = request.GET.get('q', '').strip()
 
     if not search_term:
         raise Http404()
 
-    return render(request,'recipes/pages/search.html')
+    recipes = Recipe.objects.filter(
+        Q(
+            Q(title__icontains=search_term) |
+            Q(description__icontains=search_term),
+        ),
+        is_published=True
+    ).order_by('-id')
+
+    return render(request,'recipes/pages/search.html', {
+        'page_title': f'Search for "{search_term}"',
+        'search_term': search_term,
+        'recipes': recipes
+    })  
