4b65209f93a614e2499bf4d2a3f05780af9f60f8
lucas <lucas46em@gmail.com>
2024-02-26 17:25:45 -0300

    panation css

--- aula-1/utils/pagination.py
+++ aula-1/utils/pagination.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 import math
 
 def make_pagination_range(page_range,qtd_pages,current_page):
@@ -28,3 +29,19 @@ def make_pagination_range(page_range,qtd_pages,current_page):
         'first_page_out_of_range': current_page > middle_range,
         'last_page_out_of_range': stop_range < total_pages,
     }
+
+def make_pagination(request, queryset, perpage = 9, qtd_pages = 4):
+    try:
+        current_page = int(request.GET.get('page',1))
+    except ValueError:
+         current_page = 1
+
+    paginator = Paginator(queryset, perpage)
+    page_obj  = paginator.get_page(current_page)
+
+    pagination_range = make_pagination_range(
+        paginator.page_range,
+        qtd_pages,
+        current_page,
+    ) 
+    return page_obj, pagination_range
