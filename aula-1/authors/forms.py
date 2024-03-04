528433233c9184850feebddc7e2a94abd72c56da
lucas <lucas46em@gmail.com>
2024-03-04 17:01:38 -0300

    Creating form

--- aula-1/authors/forms.py
+++ aula-1/authors/forms.py
@@ -11,4 +11,25 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password',
         ]
-
+        labels = {
+            'username': 'Username',
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'email': 'Email',
+            'password': 'Password',
+        }
+        help_texts = {
+            'email': 'The e-mail must be valid'
+        }
+        error_messages = {
+            'username': {
+                'required': 'This field must not be empty',
+                'max_length': 'This field must have less than',
+                'invalid': 'This field is invalid',
+            }
+        }
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'Type your username here'
+            }),
+        }
