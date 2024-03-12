ecd5f640b717e79395ae01722e6c9176261576d6
lucas <lucas46em@gmail.com>
2024-03-12 17:13:52 -0300

    refactoring files, init login form

--- aula-1/authors/tests/test_author_register_form.py
+++ aula-1/authors/tests/test_author_register_form.py
@@ -67,7 +67,7 @@ class AuthorRegisterFormIntegrationTest(DjangoTestCase):
     ])
     def test_fields_cannot_be_empty(self, field, msg):
         self.form_data[field] = ''
-        url = reverse("authors:create")
+        url = reverse("authors:register_create")
         response = self.client.post(url,data = self.form_data, follow=True)
 
         # self.assertIn(msg, response.content.decode('utf-8'))
@@ -75,7 +75,7 @@ class AuthorRegisterFormIntegrationTest(DjangoTestCase):
 
     def test_username_field_min_length_should_be_4(self):
         self.form_data['username'] = 'joa'
-        url = reverse("authors:create")
+        url = reverse("authors:register_create")
         response = self.client.post(url,data = self.form_data, follow=True)
 
         msg = 'Username must have at least 4 characters'
@@ -84,7 +84,7 @@ class AuthorRegisterFormIntegrationTest(DjangoTestCase):
 
     def test_username_field_max_length_should_be_150(self):
         self.form_data['username'] = 'A' * 151
-        url = reverse("authors:create")
+        url = reverse("authors:register_create")
         response = self.client.post(url,data = self.form_data, follow=True)
 
         msg = 'Username must have less than 150 characters'
@@ -93,7 +93,7 @@ class AuthorRegisterFormIntegrationTest(DjangoTestCase):
 
     def test_password_field_have_lower_upper_case_letters_and_numbers(self):
         self.form_data['password'] = 'Abc1234'
-        url = reverse("authors:create")
+        url = reverse("authors:register_create")
         response = self.client.post(url,data = self.form_data, follow=True)
 
         msg = (
@@ -105,7 +105,7 @@ class AuthorRegisterFormIntegrationTest(DjangoTestCase):
         self.assertIn(msg, response.context['form'].errors.get('password'))
 
         self.form_data['password'] = '@saSSSdasAbc1234'
-        url = reverse("authors:create")
+        url = reverse("authors:register_create")
         response = self.client.post(url,data = self.form_data, follow=True)
 
         msg = (
@@ -120,7 +120,7 @@ class AuthorRegisterFormIntegrationTest(DjangoTestCase):
         self.form_data['password'] = 'Abc1234'
         self.form_data['password_confirmation'] = 'Abc12343'
 
-        url = reverse("authors:create")
+        url = reverse("authors:register_create")
         response = self.client.post(url,data = self.form_data, follow=True)
 
         msg = 'Password and password_confirmation must be equal'
@@ -129,13 +129,13 @@ class AuthorRegisterFormIntegrationTest(DjangoTestCase):
         self.assertIn(msg, response.context['form'].errors.get('password'))
 
     def test_send_get_request_to_registration_create_view_returns_404(self):
-        url = reverse("authors:create")
+        url = reverse("authors:register_create")
         response = self.client.get(url)
 
         self.assertEqual(response.status_code,404)
 
     def test_email_field_must_be_unique(self):
-        url = reverse("authors:create")
+        url = reverse("authors:register_create")
 
         self.client.post(url,data = self.form_data, follow=True)
         response = self.client.post(url,data = self.form_data, follow=True)
@@ -143,3 +143,17 @@ class AuthorRegisterFormIntegrationTest(DjangoTestCase):
         msg = 'User email is already in use'
         self.assertIn(msg,response.context['form'].errors.get('email'))
         self.assertIn(msg, response.content.decode('utf-8'))
+
+    def test_author_created_can_login(self):
+        url = reverse("authors:register_create")
+        self.form_data.update({
+            'username':'testuser',
+            'password': '1q2w3e$R',
+            'password_confirmation': '1q2w3e$R',
+        })
+        self.client.post(url,data = self.form_data, follow=True)
+        is_autenticated = self.client.login(
+            username='testuser',
+            password='1q2w3e$R'
+        )
+        self.assertTrue(is_autenticated)
--- aula-1/authors/views.py
+++ aula-1/authors/views.py
@@ -1,15 +1,17 @@
 from django.http import Http404
 from django.shortcuts import redirect,render
 from django.contrib import messages
+from django.urls import reverse
 
-from .forms import RegisterForm
+from .forms import LoginForm, RegisterForm
 
 # Create your views here.
 def register_view(request):
     register_form_data = request.session.get('register_form_data',None)
     form = RegisterForm(register_form_data)
     return render(request, 'authors/pages/register_view.html', {
-        'form': form
+        'form': form,
+        'form_action': reverse('authors:register_create')
     })
 
 def register_create(request):
@@ -28,3 +30,13 @@ def register_create(request):
         del(request.session['register_form_data'])
 
     return redirect('authors:register')
+
+def login_view(request):
+    form = LoginForm()
+    return render(request,'authors/pages/login.html',{
+        'form':form,
+        'form_action': reverse('authors:login_create'),
+    })
+
+def login_create(request):
+    return render(request,'authors/pages/login.html')
