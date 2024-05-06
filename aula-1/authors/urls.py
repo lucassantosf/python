7dde2d91357e12f0f43ca0b95f9723c73d2f2078
lucas <lucas46em@gmail.com>
2024-05-06 12:29:10 -0300

    Init authors area

--- aula-1/authors/urls.py
+++ aula-1/authors/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('register/create/', views.register_create, name='register_create'),
     path('logout/', views.logout_view, name='logout'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('dashboard/recipe/<int:id>/edit/', views.dashboard_recipe_edit, name='dashboard_recipe_edit'),
 ]
--- aula-1/authors/views.py
+++ aula-1/authors/views.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from .forms import LoginForm, RegisterForm
 from django.contrib.auth.decorators import login_required
+from recipes.models import Recipe
 
 # Create your views here.
 def register_view(request):
@@ -45,8 +46,6 @@ def login_create(request):
         raise Http404()
 
     form = LoginForm(request.POST)
-    login_url = reverse('authors:login')
-
     if form.is_valid():
         
         authenticated_user = authenticate(
@@ -63,7 +62,7 @@ def login_create(request):
     else:
         messages.error(request, 'Invalid username or password')
 
-    return redirect(login_url)
+    return redirect(reverse('authors:dashboard'))
 
 @login_required(login_url="authors:login", redirect_field_name='next')
 def logout_view(request):
@@ -78,3 +77,36 @@ def logout_view(request):
     messages.error(request, 'Logged out successfully')
     logout(request)
     return redirect(reverse('authors:login'))
+
+@login_required(login_url="authors:login", redirect_field_name='next')
+def dashboard(request):
+    recipes = Recipe.objects.filter(
+        is_published = False,
+        author = request.user 
+    )
+    return render(
+        request, 
+        'authors/pages/dashboard.html',
+        context={
+            'recipes':recipes,
+        }
+    )
+
+@login_required(login_url="authors:login", redirect_field_name='next')
+def dashboard_recipe_edit(request,id):
+    recipe = Recipe.objects.filter(
+        is_published = False,
+        author = request.user,
+        pk=id,
+    )
+
+    if not recipe : 
+        raise Http404()
+
+    return render(
+        request, 
+        'authors/pages/dashboard_recipe.html',
+        context={
+            'recipe':recipe,
+        }
+    )
