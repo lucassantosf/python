b18007493266366c43ce99b6392f0a89c9efa851
lucas <lucas46em@gmail.com>
2024-05-08 15:51:16 -0300

    Finish crud recipes

--- aula-1/authors/urls.py
+++ aula-1/authors/urls.py
@@ -11,5 +11,7 @@ urlpatterns = [
     path('register/create/', views.register_create, name='register_create'),
     path('logout/', views.logout_view, name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('dashboard/recipe/new', views.dashboard_recipe_new, name='dashboard_recipe_new'),
+    path('dashboard/recipe/delete/', views.dashboard_recipe_delete, name='dashboard_recipe_delete'),
     path('dashboard/recipe/<int:id>/edit/', views.dashboard_recipe_edit, name='dashboard_recipe_edit'),
 ]
--- aula-1/authors/views.py
+++ aula-1/authors/views.py
@@ -106,8 +106,20 @@ def dashboard_recipe_edit(request,id):
 
     form = AuthorRecipeForm(
         data = request.POST or None,
+        files=request.FILES or None,
         instance=recipe
     )
+
+    if form.is_valid():
+        # Agora o form é válido
+        recipe = form.save(commit=False)
+        recipe.author = request.user
+        recipe.preparation_steps_is_html = False
+        recipe.is_published = False
+
+        recipe.save()
+        messages.success(request,'Sua receita foi salvo com sucesso')
+        return redirect(reverse('authors:dashboard_recipe_edit',args=(id,)))
 
     return render(
         request, 
@@ -116,3 +128,48 @@ def dashboard_recipe_edit(request,id):
             'form':form
         }
     )
+
+@login_required(login_url="authors:login", redirect_field_name='next')
+def dashboard_recipe_new(request):
+    form = AuthorRecipeForm(
+        data = request.POST or None,
+        files=request.FILES or None,
+    )
+
+    if form.is_valid():
+        # Agora o form é válido
+        recipe: Recipe = form.save(commit=False)
+
+        recipe.author = request.user
+        recipe.preparation_steps_is_html = False
+        recipe.is_published = False
+
+        recipe.save()
+        messages.success(request,'Sua receita foi salva com sucesso')
+        return redirect(reverse('authors:dashboard_recipe_edit',args=(recipe.id,)))
+
+    return render(
+        request, 
+        'authors/pages/dashboard_recipe.html',
+        context={
+            'form':form
+        }
+    )
+
+@login_required(login_url="authors:login", redirect_field_name='next')
+def dashboard_recipe_delete(request):
+    if not request.POST:
+        raise Http404()
+
+    POST = request.POST
+    id = POST.get('id')
+
+    recipe = Recipe.objects.get(
+        is_published = False,
+        author = request.user,
+        pk=id,
+    )
+
+    recipe.delete()
+    messages.success(request,'Sua receita foi deletada com sucesso')
+    return redirect(reverse('authors:dashboard'))
