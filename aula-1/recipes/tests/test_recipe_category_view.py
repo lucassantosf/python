2365a2bb9e5ef17ac9e5389deb25ea1741cd6246
lucas <lucas46em@gmail.com>
2024-05-10 17:00:41 -0300

    Finish tests

--- aula-1/recipes/tests/test_recipe_category_view.py
+++ aula-1/recipes/tests/test_recipe_category_view.py
@@ -7,13 +7,13 @@ class RecipeCategoryViewTest(RecipeTestBase):
         view = resolve(
             reverse('recipes:category', kwargs={'category_id':1})
         )
-        self.assertIs(view.func, views.category)
+        self.assertIs(view.func.view_class, views.RecipeListViewCategory)
 
-    # def test_recipe_category_view_returns_404_if_no_recipes_found(self):
-    #     response = self.client.get(
-    #         reverse('recipes:category', kwargs={'category_id':1000})
-    #     )
-    #     self.assertEqual(response.status_code, 404)
+    def test_recipe_category_view_returns_404_if_no_recipes_found(self):
+        response = self.client.get(
+            reverse('recipes:category', kwargs={'category_id':100011})
+        )
+        self.assertEqual(response.status_code, 404)
 
     def test_recipe_category_template_loads_recipes(self):
         needed_title = 'This is a category test'
@@ -29,6 +29,6 @@ class RecipeCategoryViewTest(RecipeTestBase):
         """Test recipe is_published False doesnt show"""
         recipe = self.make_recipe(is_published=False)
 
-        response = self.client.get(reverse('recipes:recipe',kwargs={'id':recipe.category.id}))
+        response = self.client.get(reverse('recipes:recipe',kwargs={'pk':recipe.category.id}))
 
         self.assertEqual(response.status_code,404)
