afeee0c3059a09fde60ad0c6b240f02127475d34
lucas <lucas46em@gmail.com>
2024-05-30 15:25:08 -0300

    tag->recipes many to many relationship

--- aula-1/tag/models.py
+++ aula-1/tag/models.py
@@ -1,8 +1,6 @@
 import string
 from random import SystemRandom
 
-from django.contrib.contenttypes.fields import GenericForeignKey
-from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.utils.text import slugify
 
@@ -12,14 +10,6 @@ class Tag(models.Model):
     slug = models.SlugField(unique=True)
 
     # Aqui começam os campo para a relação genérica
-
-    # Representa o model que queremos encaixar aqui
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    # Representa o id da linha do model descrito acima
-    object_id = models.CharField(max_length=255)
-    # Um campo que representa a relação genérica que conhece os
-    # campos acima (content_type e object_id)
-    content_object = GenericForeignKey('content_type', 'object_id')
 
     def save(self, *args, **kwargs):
         if not self.slug:
