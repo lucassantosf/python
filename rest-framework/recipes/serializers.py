8a9b4607c42ce8385767809cfb83b3e21fb4d089
lucas <lucas46em@gmail.com>
2024-06-17 17:13:58 -0300

    serializer for other fields

--- rest-framework/recipes/serializers.py
+++ rest-framework/recipes/serializers.py
@@ -1,6 +1,30 @@
 from rest_framework import serializers
+from .models import Category
+from django.contrib.auth.models import User
+from tag.models import Tag 
+
+class TagsSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField(max_length=255)
+    slug = serializers.SlugField(max_length=255)
 
 class RecipeSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=65)
     description = serializers.CharField(max_length=165)
+    public = serializers.BooleanField(source='is_published')
+    preparation = serializers.SerializerMethodField()
+    category =  serializers.StringRelatedField()
+    author =  serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all()
+    )
+    tags = serializers.PrimaryKeyRelatedField(
+        queryset=Tag.objects.all(),
+        many=True
+    )
+    tag_objects = TagsSerializer(
+        many=True, source='tags'
+    )
+
+    def get_preparation(self,recipe):
+        return f'{recipe.preparation_time} {recipe.preparation_time_unit}' 
