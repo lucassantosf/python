ab94e24d1e1b54071e08cf0e29e04686945f3c9a
lucas <lucas46em@gmail.com>
2024-06-18 17:51:36 -0300

    HyperlinkedRelatedField

--- rest-framework/recipes/serializers.py
+++ rest-framework/recipes/serializers.py
@@ -1,30 +1,39 @@
-from rest_framework import serializers
-from .models import Category
 from django.contrib.auth.models import User
-from tag.models import Tag 
+from rest_framework import serializers
+from tag.models import Tag
 
-class TagsSerializer(serializers.Serializer):
+
+class TagSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     name = serializers.CharField(max_length=255)
-    slug = serializers.SlugField(max_length=255)
+    slug = serializers.SlugField()
+
 
 class RecipeSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=65)
     description = serializers.CharField(max_length=165)
     public = serializers.BooleanField(source='is_published')
-    preparation = serializers.SerializerMethodField()
-    category =  serializers.StringRelatedField()
-    author =  serializers.PrimaryKeyRelatedField(
+    preparation = serializers.SerializerMethodField(
+        method_name='any_method_name'
+    )
+    category = serializers.StringRelatedField()
+    author = serializers.PrimaryKeyRelatedField(
         queryset=User.objects.all()
     )
     tags = serializers.PrimaryKeyRelatedField(
         queryset=Tag.objects.all(),
         many=True
     )
-    tag_objects = TagsSerializer(
+    tag_objects = TagSerializer(
         many=True, source='tags'
     )
+    tag_links = serializers.HyperlinkedRelatedField(
+        many=True,
+        source='tags',
+        queryset=Tag.objects.all(),
+        view_name='recipes:recipes_api_v2_tag'
+    )
 
-    def get_preparation(self,recipe):
-        return f'{recipe.preparation_time} {recipe.preparation_time_unit}' 
+    def any_method_name(self, recipe):
+        return f'{recipe.preparation_time} {recipe.preparation_time_unit}'
