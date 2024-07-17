d08ef14a1186c47ad34257edc299583669807739
lucas <lucas46em@gmail.com>
2024-07-17 16:09:06 -0300

    cors configs

--- rest-framework/project/settings/installed_apps.py
+++ rest-framework/project/settings/installed_apps.py
@@ -6,6 +6,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # CORS Headers
+    "corsheaders",
     # Django Rest Framework
     'rest_framework',
     'rest_framework_simplejwt',
