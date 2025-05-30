9c15d40a62a0f73ec0e6a8ea572c9f05a621a90a
lucas <lucas46em@gmail.com>
2025-05-30 19:01:16 -0300

    init app

---
+++ ai_llm_engineering_course/projects/3_email_bot/app.py
@@ -0,0 +1,19 @@
+from openai import OpenAI
+from reader import EmailReader
+from sender import EmailSender
+
+class LLMModel:
+    def __init__(self):
+        self.client = OpenAI(base_url="http://localhost:11434/v1",api_key="ollama")
+        self.model_name = "llama3.2:1b"
+
+    def generate_completion(self,messages):
+        try:
+            response = self.client.chat.completions.create(
+                model=self.model_name,
+                messages=messages,
+                temperature=0.0
+            )
+            return response.choices[0].message.content 
+        except Exception as e:
+            return f"Error generating response: {str(e)}" 
