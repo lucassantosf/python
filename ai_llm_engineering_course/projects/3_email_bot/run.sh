a605d87209b1cc195b3e78f4e815a25227261312
lucas <lucas46em@gmail.com>
2025-06-24 18:12:18 -0300

    removed not used projects

--- ai_llm_engineering_course/projects/3_email_bot/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while true; do
-  echo "Running email bot... $(date +"%Y-%m-%d %H:%M:%S")"
-  python app.py
-  echo "Waiting for next run... $(date +"%Y-%m-%d %H:%M:%S")"
-  sleep 60
-done
